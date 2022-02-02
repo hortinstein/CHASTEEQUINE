a7f311cf1eaa70129359e8dfb4fce78545786e7b
Alex Hortin <hortinstein@gmail.com>
2022-02-02 10:33:27 -0500

    Update client.nim

--- client.nim
+++ client.nim
@@ -1,7 +1,23 @@
-import asyncdispatch, httpclient
+#import asyncdispatch, httpclient
 
-proc asyncProc(): Future[string] {.async.} =
-  var client = newAsyncHttpClient()
-  return await client.getContent("http://127.0.0.1:8080")
+#proc asyncProc(): Future[string] {.async.} =
+#  var client = newAsyncHttpClient()
+#  return await client.getContent("http://127.0.0.1:8080")
 
-echo waitFor asyncProc()
+# echo waitFor asyncProc()
+
+#import asyncdispatch, httpclient
+
+#proc asyncProc(): Future[string] {.async.} =
+#  var client = newAsyncHttpClient()
+#  return await client.getContent("https://8080-hortinstein-chasteequine-ep8qmltidfj.ws-us30.gitpod.io/")
+
+#echo waitFor asyncProc()
+
+# import harpoon
+# import std/asyncdispatch      # Async works. Can work with Threads, Tasks, Macros, etc.
+# import std/json               # GET and POST from JSON to JSON directly.
+# from std/uri import parseUri  # To use Uri.
+
+# echo getContent(parseUri"http://8080-hortinstein-chasteequine-ep8qmltidfj.ws-us30.gitpod.io/")
+  
